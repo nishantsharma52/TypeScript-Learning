ccab8ac552396d58685faa931c2236c7f200d8c2
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-18 15:23:58 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person:string):string{
+    return `hello ${person} , welcome to Chaicode`
+}
+ const username:string = "Chai Aur TypeScript";
+ console.log(greet(username));
+ 
---
+++ src/typesinTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : "6"
+
+let channelName = "ChaiAurCode";
+
+
+let chaiFlavour:string = "masala chai";
+chaiFlavour = "ginger tea";
+
+let chaiOrder:boolean = true;
