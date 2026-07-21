9ac29edcfbdf7e694f6234654281676a2a59efb0
Nishant Sharma <nishantsharma5330@gmail.com>
2026-07-21 20:44:05 +0530

    updated code

---
+++ reactTs/src/hooks/useFetch.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+export function useFetch<T>(url: string): FetchState<T> {
+  const [state, setState] = useState<FetchState<T>>({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    // To make fetch request using url
+  }, [url]);
+
+  return state;
+}
